e8185bd8753d75479a438e46a6c23b0416885656
Codes Priyansh <codespriyansh@gmail.com>
2024-05-18 14:20:11 +0530

    Add files via upload

---
+++ challenge-46.py
@@ -0,0 +1,13 @@
+#solutions for challange - 46
+
+#Q1 temp = 9
+
+#Q2 the bubble sort has not worked
+
+#Q3 selection
+
+#Q4 temp, j, swapped 
+
+#Q5 insertion 
+
+#Q6 if the array is short, a ubble sort will be quicker to program 
