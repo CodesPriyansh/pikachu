9186c6b0d13c8fb3680dce64f0832da4a5cba02e
Codes Priyansh <codespriyansh@gmail.com>
2024-05-18 14:09:59 +0530

    Add files via upload

---
+++ challenge-45.py
@@ -0,0 +1,13 @@
+# solutions for challenge - 45
+
+#Q1
+discounts = [["summer10", 0.1], ["welcome", 0.15], ["refer20", 0.2], ["loyalty25", 0.25]] 
+
+#Q2 data type of valid is bool.
+
+#Q3 
+file = open("codes.txt", "r")
+discount = eval(file.read())
+file.close() 
+
+#Q4 shorter programs, many people can work on the same program - writing different functions, etc. 
